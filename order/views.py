948a3a1a4339da4104fb751d787bcc42f9b90557
Ahmane Samir <samirahmane.trading@gmail.com>
2021-10-22 20:13:43 +0100

    feat: removed strip info

--- order/views.py
+++ order/views.py
@@ -25,25 +25,17 @@ def checkout(request):
     serializer = OrderSerializer(data=request.data)
 
     if serializer.is_valid():
-        stripe.api_key = settings.STRIPE_SECRET_KEY
+        print("checkout data:\n",serializer.validated_data)
         paid_amount = sum(
             item.get("quantity") * item.get("product").price
             for item in serializer.validated_data["items"]
         )
 
-        try:
-            charge = stripe.Charge.create(
-                amount=int(paid_amount * 100),
-                currency="USD",
-                description="Charge from Djackets",
-                source=serializer.validated_data["stripe_token"],
-            )
 
-            serializer.save(user=request.user, paid_amount=paid_amount)
+        serializer.save(user=request.user, paid_amount=paid_amount)
 
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        except Exception:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
